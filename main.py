7390999ff1b7b4ee42f5d75b57f90d3fd3f74ebe
Thomas Margnac <t.margnac@gmail.com>
2022-12-10 18:47:31 +0100

    Just printing the first 5 rows

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
 	bitcoin = Price("bitcoin")
 	time1, price1, lower1, higher1, circulatingSupply1 = bitcoin.get()
 	time2, price2, lower2, higher2, circulatingSupply2 = bitcoin.get()
-	print(bitcoin.dataframe)
+	print(bitcoin.dataframe.head(5))
